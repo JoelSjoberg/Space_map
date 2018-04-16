49e1a8ee848930ef13f670ea0243768efa31f008
Joel Sjöberg <joelsjoberg3@gmail.com>
2018-04-16 14:33:08 +0300

    TestCommit
    Testing commit from git desktop

---
+++ Space_map/Assets/Scripts/HoverDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverDetect : MonoBehaviour {
+
+
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = transform.localScale * 2f;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = transform.localScale / 2f;
+    }
+}
